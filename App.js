7feec75278a7d23c2a571fe17d2e0f96ebf57c5f
Georgi Dimitrov <91046490+gdimit01@users.noreply.github.com>
2023-02-12 00:18:13 +0000

    Add files via upload
    This is the start of me learning how to use React Native for iOS and Android

---
+++ App.js
@@ -0,0 +1,23 @@
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add Goal' />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  
+});
